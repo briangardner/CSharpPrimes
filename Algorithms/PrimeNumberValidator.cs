aa6bf24f2e1559859aa32b21c1164258d5300a7d
Brian Gardner <1761566+briangardner@users.noreply.github.com>
2018-08-07 16:09:18 -0400

    Generating first 100 primes

---
+++ Algorithms/PrimeNumberValidator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class PrimeNumberValidator
+    {
+
+        public bool IsPrime(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+
+            if (number == 2)
+            {
+                return true;
+            }
+
+            if (number % 2 == 0)
+            {
+                return false;
+            }
+
+            var ceiling = Math.Sqrt(number);
+            for (int i = 3; i <= ceiling; i += 2)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
 
 namespace Algorithms
 {
@@ -10,6 +11,41 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
+            var validator = new PrimeNumberValidator();
+            List<int> primes = new List<int>();
+            int number = 2;
+            do
+            {
+                if (validator.IsPrime(number))
+                {
+                    primes.Add(number);
+                }
+
+                number++;
+            } while (primes.Count < 100);
+
+            foreach (var prime in primes)
+            {
+                Console.WriteLine(prime);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static bool ShouldContinue()
+        {
+            do
+            {
+                Console.Write("Continue y/n:");
+                var input = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                if (input != 'y' && input != 'n')
+                {
+                    continue;
+                }
+
+                return input == 'y';
+            } while (true);
         }
     }
 }
